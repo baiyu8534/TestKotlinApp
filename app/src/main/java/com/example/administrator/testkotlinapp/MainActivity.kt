7a3143887831f815564a7505fb48dc5e82df5186
baiyu8534 <657116885@qq.com>
2017-07-24 16:11:53 +0800

    普通的点击事件，弹toast

--- app/src/main/java/com/example/administrator/testkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/administrator/testkotlinapp/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.administrator.testkotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        initListener()
     }
+
+    fun initView(){
+        textView = findViewById(R.id.tv) as TextView
+    }
+
+    fun initListener(){
+        textView?.setOnClickListener {
+            Toast.makeText(this,"sdfasdf",Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
